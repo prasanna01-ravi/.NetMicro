aac8ab775ef1c113ca199b16d0184321c04b837d
SANNA-PC\User <prasanna01.raveendran@gmail.com>
2023-02-03 22:56:09 +0530

    Project Reference added, connection string and seedings added to discount grpc project

--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -1,14 +1,21 @@
 
+using Discount.Business.data;
+using Discount.Business.Extensions;
+using Discount.Business.repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountContext, DiscountContext>();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
-
+app.MigrateDatabase();
 // Configure the HTTP request pipeline.
 //app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
